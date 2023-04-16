1e5b0ba28637c496211e00860a40d47e23926c91
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-04-16 20:58:18 +0300

    feat: add sights model

--- db/migrate/20230416175529_create_sights.rb
+++ db/migrate/20230416175529_create_sights.rb
@@ -2,7 +2,7 @@ class CreateSights < ActiveRecord::Migration[7.0]
   def change
     create_table :sights do |t|
       t.references :place, null: false, foreign_key: true
-      t.string :activity_type
+      t.string :activity_type, null: false
 
       t.timestamps
     end
